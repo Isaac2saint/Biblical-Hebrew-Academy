2e11992e7a29f84a462799cc922537ea2cc129bd
Isaac2saint <Isaac2saint@gmail.com>
2025-01-09 21:39:16 +0000

    1 hour flashcard app

---
+++ app.js
@@ -0,0 +1,69 @@
+flashcard = document.getElementById('flashcard');
+// previous_btn = document.getElementById('previous');
+next_btn = document.getElementById('next');
+definition_input = document.getElementById('consonant-def');
+right_or_wrong = document.getElementById('right-or-wrong');
+// let previousConsonant = '';
+// let currentConsonant = '';
+const consonants = {
+    'א': 'Aleph',
+    'ב': 'Bet',
+    'ג': 'Gimel',
+    'ד': 'Dalet',
+    'ה': 'He',
+    'ו': 'Vav',
+    'ז': 'Zayin',
+    'ח': 'Het',
+    'ט': 'Tet',
+    'י': 'Yod',
+    'כ': 'Kaf',
+    'ל': 'Lamed',
+    'מ': 'Mem',
+    'נ': 'Nun',
+    'ס': 'Samekh',
+    'ע': 'Ayin',
+    'פ': 'Pe',
+    'צ': 'Tsade',
+    'ק': 'Qof',
+    'ר': 'Resh',
+    'ש': 'Shin',
+    'ת': 'Tav'
+  };
+
+let randomName = '';
+
+function displayRandomConsonant() {
+    // previousConsonant = currentConsonant;
+    // const consonants_obj = ['א', 'ב', 'ג', 'ד', 'ה', 'ו', 'ז', 'ח', 'ט', 'י', 'כ', 'ל', 'מ', 'נ', 'ס', 'ע', 'פ', 'צ', 'ק', 'ר', 'ש', 'ת'];
+    let letters = Object.keys(consonants);
+      
+    let randomConsonantIndex = Math.floor(Math.random() * letters.length);
+    let randomLetter = letters[randomConsonantIndex];
+    randomName = consonants[randomLetter];
+    // currentConsonant = randomConsonant;
+  flashcard.innerHTML = randomLetter;
+//   console.log(previousConsonant);
+}
+
+next_btn.addEventListener('click', displayRandomConsonant);
+
+definition_input.addEventListener('keyup', function(event) {
+    if (event.key === 'Enter') {
+        let definition = definition_input.value;
+        definition_input.value = '';
+        console.log(definition);
+        console.log(randomName);
+        if (definition === randomName) {
+            console.log('Correct!');
+            
+            displayRandomConsonant();
+        }
+        else {
+            console.log('Incorrect!');
+            right_or_wrong.innerHTML = 'Incorrect!';
+        }
+        // displayRandomConsonant();
+    }
+});
+
+displayRandomConsonant();
